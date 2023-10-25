3dfec9afb3b959d68507c5ca43439fe4cef76936
Svetlana <A.svet.mr@yandex.ru>
2023-10-25 16:00:17 +0300

    v1

--- src/main/java/com/example/demo3/HelloController.java
+++ src/main/java/com/example/demo3/HelloController.java
@@ -1,14 +1,32 @@
-package com.example.demo3;
+                                                                                                                               package com.example.demo3;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.List;
+
+                                                                                                                               public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void loadFile(ActionEvent event) {
+        String fname = textFileName.getText;
+        try {
+            List<String> lines = loadTextFromFile(fname);
+            showText(Lines);
+        } catch (IOException e) {
+            contens.setText("Файл не найден");
+        }
+
+    }
+    private static  List<String> loadTextFromFile(String fname) throws IOException{
+        List<String> lines = Files.readAllLines(Path.of(fname));
+        lines.sort(Comparator.naturalOrder());
     }
 }
